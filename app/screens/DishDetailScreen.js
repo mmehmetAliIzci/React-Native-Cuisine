61a4f2f9160bf86237185131d6b632a68028c5ff
MEHMET ALI IZCI <mmehmetaliizci@hotmail.com>
2018-02-16 10:49:44 +0100

    Remove DishDetailContainer / Add DishDetailScreen

--- app/screens/DishDetailScreen.js
+++ app/screens/DishDetailScreen.js
@@ -1,50 +1,58 @@
+/**
+ * Responsible for rendering DetailScreen by calling child views
+ */ 
+
 import React, { Component } from 'react';
-import {
-  ScrollView,         // Scrollable container
-  StyleSheet,         // CSS-like styles
-  View                // Container component
-} from 'react-native';
-import Dimensions from 'Dimensions';
-import TextWithFont from '../components/TextWithFont'
+import { ScrollView,View,StyleSheet } from "react-native";
+
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as actions from '../actions/dishesActions';
 
 import DishLabels from '../components/DishLabels'
 import DishMethod from '../components/DishMethod'
-import DishImage from '../components/DishImage'
+import DishDetailImage from '../components/DishDetailImage'
 import SectionHeading from '../components/SectionHeading'
 import DishIngredients from '../components/DishIngredients'
 
+class DishDetailScreen extends Component {
 
+  render() {
 
-
-export default class Movie extends Component {
-
-
-
-
-  // Extract movie object passed as a prop from Row component
-  render({ dish } = this.props) {
-    // Extract values from movie object
     return (
       <ScrollView style={styles.container}>
         <View style={styles.imageContainer}>
-          <DishImage dish={dish}/>
+          <DishDetailImage dish={this.props.Dishes.selectedDish}/>
         </View>
         <View style={styles.textContentContainer}>
           <View style={[styles.sectionContainer,styles.labelsContainer]}>
-            <DishLabels dish={dish}/>
+            <DishLabels dish={this.props.Dishes.selectedDish}/>
           </View>
           <View style={[styles.sectionContainer,styles.ingredientsContainer]}>
-            <DishIngredients ingredients={dish.ingredients}/>
+            <DishIngredients ingredients={this.props.Dishes.selectedDish.ingredients}/>
           </View>
           <View style={[styles.sectionContainer,styles.methodContainer]}>
-            <DishMethod method={dish.method}/>
+            <DishMethod method={this.props.Dishes.selectedDish.method}/>
           </View>
         </View>
       </ScrollView>
     );
   }
+
 }
 
+
+function mapStateToProps(state) {
+  return {
+    Dishes: state.Dishes
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+}
 const styles = StyleSheet.create({
   // Main container
   container: {
@@ -73,3 +81,8 @@ const styles = StyleSheet.create({
     paddingBottom: 20
   }
 });
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DishDetailScreen);
